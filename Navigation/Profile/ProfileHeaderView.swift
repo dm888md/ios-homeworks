fb2ef53b9fee2069d512e6dfb7286650819a1c51
“dm1973” <dm1973@yandex.ru>
2024-04-04 21:54:46 +0500

    Добавлены элементы

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -22,8 +22,7 @@ class ProfileHeaderView: UIView {
     let statusLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        //        label.textColor = .gray
-        label.textColor = .red
+        label.textColor = .gray
         label.text = "Waiting for something..."
         label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -37,9 +36,7 @@ class ProfileHeaderView: UIView {
         button.setTitle("Show status", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-//        button.frame = CGRect(x: 0, y: 0, width: 300, height: 50)
-
-        button.frame = CGRect(x: 40, y: 440, width: 222, height: 50)
+        button.frame = CGRect(x: 0, y: 0, width: 10, height: 50)
 
         button.layer.shadowOffset.width = 3
         button.layer.shadowOffset.height = 4
@@ -59,10 +56,8 @@ class ProfileHeaderView: UIView {
     let myFoto: UIImageView = {
         let foto = UIImageView ()
         foto.image = UIImage(named: "profileImage")
-        foto.frame = CGRect(x: 16, y: 50+16, width: 140, height: 140)
-//        foto.frame = CGPoint(x: 100, y: 100)
-
-        foto.layer.cornerRadius = 70
+        foto.frame = CGRect(x: 16, y: 16, width: 120, height: 120)
+        foto.layer.cornerRadius = 60
         foto.clipsToBounds = true
         foto.layer.borderColor = UIColor.white.cgColor
         foto.layer.borderWidth = 3
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -25,49 +25,48 @@ class ProfileViewController: UIViewController {
         self.profileHeaderView.addSubview(profileHeaderView.myButton)
         self.profileHeaderView.addSubview(profileHeaderView.myFoto)
 
+
+
 // Поле с именем - готово
-        let fullNameLabelTopAnchor = profileHeaderView.fullNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 27)
-        let fullNameLabelCenter = profileHeaderView.fullNameLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
-  
+        let fullNameLabelTopAnchor: () = profileHeaderView.fullNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 27).isActive = true
+        let fullNameLabelCenter: () = profileHeaderView.fullNameLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+
 //        Поле со статусом - готово
-        let stastusLabelTopAnchor = profileHeaderView.statusLabel.bottomAnchor.constraint(equalTo: profileHeaderView.myButton.topAnchor, constant: -34)
-        let stastusLabelCenter = profileHeaderView.statusLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        let stastusLabelTopAnchor: () = profileHeaderView.statusLabel.bottomAnchor.constraint(equalTo: profileHeaderView.myButton.topAnchor, constant: -34).isActive = true
+        let stastusLabelCenter: () = profileHeaderView.statusLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
 
-//        Настройка фото
-        let myFotoTopAnchor = profileHeaderView.myFoto.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16)
-        let myFotoLeftAnchor = profileHeaderView.myFoto.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 16)
+        profileHeaderView.myFoto.frame = CGRect(
+            x: self.view.frame.minX + 16,
+            y: self.view.frame.minY + 100 + 16, // не получается задать коордиты от safeAreaLayoutGuide
+            width: profileHeaderView.myFoto.frame.width,
+            height: profileHeaderView.myFoto.frame.height)
 
-//        Настройка кнопки
-        let myButtomTopAnchor = profileHeaderView.myButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0)
-        let myButtonLeftAnchor = profileHeaderView.myButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
-
-        profileHeaderView.myButton.frame = CGRect(x: 16, y: 50 + 16+16+profileHeaderView.myFoto.frame.height, width: 300, height: 50)
-
-        profileHeaderView.myFoto.frame = CGRect(x: 20 + 16, y: 100 + 16, width: profileHeaderView.myFoto.frame.width, height: profileHeaderView.myFoto.frame.height)
-//
+        profileHeaderView.myButton.frame = CGRect(
+            x: self.view.frame.minX + 16,
+            y: self.profileHeaderView.myFoto.frame.maxY + 10,
+            width: self.view.frame.width - 32,
+            height: profileHeaderView.myButton.frame.height)
 
 
-        NSLayoutConstraint.activate([fullNameLabelTopAnchor, fullNameLabelCenter, stastusLabelTopAnchor,stastusLabelCenter, myFotoTopAnchor, myFotoLeftAnchor, myButtomTopAnchor, myButtonLeftAnchor])
+
+//        NSLayoutConstraint.activate([fullNameLabelTopAnchor, fullNameLabelCenter, stastusLabelTopAnchor,stastusLabelCenter, myFotoTopAnchor, myFotoLeftAnchor, myButtomTopAnchor, myButtonLeftAnchor, myButtonRightAnchor])
 
     }
 
 //    override func viewWillLayoutSubviews () {
 //        super.viewWillLayoutSubviews ()
 //        profileHeaderView.frame = CGRect(
-//            x: 0,
-//            y: 0,
+//            x: profileHeaderView.frame.minX,
+//            y: profileHeaderView.frame.minY,
 //            width: view.frame.size.width - 20,
 //            height: view.frame.size.height - 20)
 //    }
-//   
+   
     override func viewWillLayoutSubviews () {
-        super.viewWillLayoutSubviews ()
         profileHeaderView.frame = view.frame
+        super.viewWillLayoutSubviews ()
+
     }
 
-
-
-
-    
 }
 
